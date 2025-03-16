0eea5fce12f0c97dff69c176175cdc9ca064442e
vandadiReddyRaju <140302379+vandadiReddyRaju@users.noreply.github.com>
2025-03-16 22:26:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     formData.append('file', file);
 
     try {
-        const response = await fetch('https://rct-backend.onrender.com/api/process', {
+        const response = await fetch('http://127.0.0.1:5000/api/process', {
             method: 'POST',
             body: formData,
         });
